5bf0f532b572ddedadea7d43817ee03a19604e4b
Guillermo Rauch <rauchg@gmail.com>
2013-04-05 08:13:39 -0700

    index: fix computed style lookeup

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 var prop = require('transform-property');
 if (!prop) return module.exports = false;
 
+var map = {
+  webkitTransform: '-webkit-transform',
+  OTransform: '-o-transform',
+  msTransform: '-ms-transform',
+  MozTransform: '-moz-transform',
+  transform: 'transform'
+};
+
 // from: https://gist.github.com/lorenzopolidori/3794226
-var el = document.createElement('p');
+var el = document.createElement('div');
 el.style[prop] = 'translate3d(1px,1px,1px)';
-var has3d = window.getComputedStyle(el).getPropertyValue(prop);
-module.exports = null != has3d && has3d.length && 'none' != has3d;
+document.body.insertBefore(el, null);
+var val = window.getComputedStyle(el).getPropertyValue(map[prop]);
+document.body.removeChild(el);
+module.exports = null != val && val.length && 'none' != val;
